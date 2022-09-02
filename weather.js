cc0eccfc102b031ceb48a6b8e67847bddae1e422
Cobalt-Velvet <106420073+Cobalt-Velvet@users.noreply.github.com>
2022-09-02 18:55:04 +0900

    Add files via upload

--- weather.js
+++ weather.js
@@ -2,17 +2,34 @@ function getWeather() {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=kita,jp&appid=10a590f15fae9a0e10e5b07fd4305eae&units=metric&lang=kr')
     .then(response => response.json())
     .then(function(json) {
-      let city = document.getElementById('city');
-      let weather = document.getElementById('weather');
       let temperature = document.getElementById('temperature');
+      let feels_like = document.getElementById('feels_like');
+      let city = document.getElementById('city');
+
+      let weather = document.getElementById('weather');
+      let description = document.getElementById('description');
       let humidity = document.getElementById('humidity');
-      let pressure = document.getElementById('pressure');
-    
-      city.textContent = json.name;
-      weather.textContent = json.weather[0].main;
+      let clouds = document.getElementById('clouds');
+
+      let icon = document.getElementById('icon');
+      let sunrise = document.getElementById('sunrise');
+      let sunset = document.getElementById('sunset');
+      
       temperature.textContent = json.main.temp;
+      feels_like.textContent = json.main.feels_like;
+      city.textContent = json.name;
+
+      weather.textContent = json.weather[0].main;
+      description.textContent = json.weather[0].description;
       humidity.textContent = json.main.humidity;
-      pressure.textContent = json.main.pressure;
+      clouds.textContent = json.clouds.all;
+
+      icon.textContent = json.weather[0].icon;
+      sunrise.textContent = json.sys.sunrise;
+      sunset.textContent = json.sys.sunset;
+
+      
+      
     })
     .catch(function(error) {
       console.log('ERROR: ' + error);
@@ -21,5 +38,5 @@ function getWeather() {
 
   window.addEventListener('load', function() {
     getWeather()
-    setInterval(getWeather,500);
+    setInterval(getWeather,60000);
 });
